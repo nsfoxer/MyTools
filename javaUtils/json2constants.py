9c15a61a6e6968017421b7fedce01a8889add158
muwuren <1261805497@qq.com>
2021-12-21 20:26:14 +0800

    增加interface和\t功能

--- javaUtils/json2constants.py
+++ javaUtils/json2constants.py
@@ -1,26 +1,28 @@
 #!/bin/python
 import json
 
-def display(a: str):
+def display(a: str, depth: int):
     if len(a) == 0:
         return
-    s = "private String "
+    s = "\t"*depth + "private String "
     for c in a:
         if c.isupper():
             s += "_"
         s += c.upper()
     print(s+";")
-    
 
-
-def parse(d:dict):
+def parse(d:dict, depth:int):
     for a in d:
-        display(a)
         if type(d[a]) == dict:
-            parse(d[a])
+            s = "\t"*depth
+            print(f"{s}interface {a} {{")
+            parse(d[a], depth+1)
+            print(f"{s}}}")
+        else:
+            display(a, depth)
 
 
 with open("./1.json", "r") as f:
     data = f.read()
     d = json.loads(data)
-    parse(d)
+    parse(d, 0)
